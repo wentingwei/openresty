843f1e0b153dac97a6118bceba631bd8466361c1
Tingwei Wen <wentingwei@qq.com>
2017-04-09 13:25:32 +0800

    init openresty dir

---
+++ lua/common/param.lua
@@ -0,0 +1,14 @@
+local _M = {}
+function _M.is_number( ... )
+	-- body
+	local arg = {...}
+	local num
+	for _,v in ipairs(arg) do
+		num = tonumber(v)
+		if not num then 
+			return false
+		end
+	end
+	return true
+end
+return _M
